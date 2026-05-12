8a7d529f87652db37cb2b7fb5f0ac1d5793da435
Scott Makos <makosscott@gmail.com>
2026-05-12 18:04:11 -0400

    Adding files to path

---
+++ Python/Tokenization practice.py
@@ -0,0 +1,46 @@
+
+# imports 
+import urllib.request # Extensible library for opening URLs
+import re # Regular expression library for string manipulation
+
+url = ("https://raw.githubusercontent.com/rasbt/LLMs-from-scratch/main/ch02/01_main-chapter-code/the-verdict.txt") #website URL
+
+file_path = "the-verdict.txt" #name of the file which will be saved locally
+urllib.request.urlretrieve(url, file_path) #download the file from the URL and save it locally with the specified file name
+
+with open("the-verdict.txt", "r", encoding="utf-8") as f: #open the file in read mode with UTF-8 encoding
+    rawtext = f.read() #read the contents of the file and save them to rawtext variable
+print("Total number of character:", len(rawtext))
+print(rawtext[:99])
+
+#start of tokenization 
+
+
+ #split the text into tokens based on whitespace'. r' is used to treat the string as a raw string (for \ to be treated literally). (\s) means any whitespace chraracter. 
+preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', rawtext)
+preprocessed = [item.strip() for item in preprocessed if item.strip()] #remove leading and trailing whitespace from each token and filter out empty tokens
+print(len(preprocessed)) #print the number of tokens
+print(preprocessed[:30]) #print first 30 tokens just for visual check.
+
+#now that we have tokenized the rawtext, we want to create a aphabetically sorted list of unique tokens. Which will then be assigned an Token ID.
+allwords = sorted(set(preprocessed)) #create a sorted list of unique tokens (set() is used to guarantee uniqueness.)
+vocab_size = len(allwords) #calculate the vocabulary size (number of *unique* tokens)
+print("Vocabulary size:", vocab_size) #print the vocabulary size
+
+#We now begin the process of creating the tokenizer class.
+class SimpleTokenizerV1:
+    def __init__(self, vocab):
+        self.str_to_int = vocab #store the provided vocabulary mapping from string to integer as an instance variable called str_to_int
+        self.int_to_str = {i: s for s, i in vocab.items()} #create a reverse mapping from integer to string using a dictionary comprehension
+
+    def encode(self, text):
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text) #split the input text into tokens using the same regular expression as before
+                                                                     # if item.strip() returns FALSE if the string is empty or contains only whitespace, and TRUE otherwise. 
+        preprocessed = [item.strip() for item in preprocessed if item.strip()] #remove leading and trailing whitespace from each token and filter out empty tokens
+        ids = [self.str_to_int[s] for s in preprocessed] #convert each token to its corresponding integer ID using the str_to_int mapping
+        return ids
+    
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids]) #join the list of tokens into a single string with spaces in between
+        text = re.sub(r'\s([,.:;?_!"()\']|--)\s', r'\1', text) #remove spaces around punctuation marks using a regular expression substitution
+        return text
